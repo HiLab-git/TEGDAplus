4ed09a88dd76837134257eb6588db3ed17c0fc76
SherlockZYB <1015462621@qq.com>
2026-08-27 14:26:29 +0800

    Add BraTS MEN TEGDA+ run command

--- code/run_tegda+.sh
+++ code/run_tegda+.sh
@@ -7,6 +7,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
 
 python test_time_adaptation_3D_online_eval.py --target_domain BraTS_PED --TTA_method tegda_plus --exp BraTs2023_GLI2PED_TEGDAplus
+python test_time_adaptation_3D_online_eval.py --target_domain BraTS_MEN --TTA_method tegda_plus --exp BraTs2023_GLI2MEN_TEGDAplus
 python test_time_adaptation_2D_online_eval.py --target_domain B --TTA_method tegda_plus --exp MMS_A2B_TEGDAplus
 python test_time_adaptation_2D_online_eval.py --target_domain C --TTA_method tegda_plus --exp MMS_A2C_TEGDAplus
 python test_time_adaptation_2D_online_eval.py --target_domain D --TTA_method tegda_plus --exp MMS_A2D_TEGDAplus
